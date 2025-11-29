7402905797dc0eb08e7216b948607eef464f79f8
PH68503 <duonghoangzxc@gmail.com>
2025-11-29 11:00:15 +0700

    Code trên lớp 29/11

--- Code_tren_lop/Code_tren_lop/Program.c
+++ Code_tren_lop/Code_tren_lop/Program.c
@@ -5,27 +5,37 @@
 
 int main()
 {
-    printf("Hi there!\n");
+	int i;
+	int intergerArray[100];
+	int length;
+	int tmp;
+	printf("Nhap do dai mang: ");
+	scanf("%d", &length);
+	printf("Nhap du lieu mang %d phan tu:\n ", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d]= ", i);
+		scanf("%d", &intergerArray[i]);
+	}
+	printf("Sap xep mang tang dan %d phan tu:\n ", length);
+	for (int i = 0; i < length - 1; i++)
+	{
+		if (intergerArray[i] > intergerArray[i + 1])
+		{
+			tmp = intergerArray[i];
+			intergerArray[i] = intergerArray[i + 1];
+			intergerArray[i + 1] = tmp;
+			i = -1;
+		}
+	}
+	printf("Xuat du lieu mang %d phan tu:\n ", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d]= %d\n", i, intergerArray[i]);
+	}
+	printf("Xuat mang du lieu %d phan tu theo tu tu Giam dan:\n", length);
+	for (i = length - 1; i > -1; i--)
+	{
+		printf("mang[%d]= %d\n", i, intergerArray[i]);
+	}
 }
-
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
-
-// Danh cho nguoi moi bat dau:
-//   1. Mo "View" > "Solution Explorer"
-//          de them/quan ly cac files
-//   2. Mo "View" > "Output"
-//          de kiem tra "build output" va "cac thong bao khac"
-//   3. Mo "View" > "Error List"
-//          de xem loi
-//   4. Chon Project > "Add" > "New Item"
-//          de tao moi file code,
-//      hoac Project > "Add" > "Existing Item"
-//          de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//          chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//          trong cua so "Solution Explorer"
-//          bam chuot phai vao "Solution"
-//          chon "Open Folder in File Explorer"
-
-// GV: AnhTT184
